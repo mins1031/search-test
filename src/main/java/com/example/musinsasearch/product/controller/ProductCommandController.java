1df143d58d24523195566e740851c3fc72dad8bb
mins1031 <urisegea@naver.com>
2022-06-27 00:30:12 +0900

    feat: 예외 핸들러 추가 및 http 파일 추가

--- src/main/java/com/example/musinsasearch/product/controller/ProductCommandController.java
+++ src/main/java/com/example/musinsasearch/product/controller/ProductCommandController.java
@@ -1,18 +1,26 @@
 package com.example.musinsasearch.product.controller;
 
+import com.example.musinsasearch.brand.exception.NotFoundBrandException;
+import com.example.musinsasearch.category.exception.NotFoundCategoryException;
 import com.example.musinsasearch.common.response.RestResponse;
 import com.example.musinsasearch.product.dto.request.ProductCreateRequest;
 import com.example.musinsasearch.product.dto.request.ProductDeleteRequest;
 import com.example.musinsasearch.product.dto.request.ProductUpdateRequest;
+import com.example.musinsasearch.product.exception.NotFoundProductException;
 import com.example.musinsasearch.product.service.ProductCommandService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ProductCommandController {
@@ -26,7 +34,7 @@ public class ProductCommandController {
         return RestResponse.of(HttpStatus.OK, null);
     }
 
-    @PatchMapping(ProductCommandControllerPath.PRODUCT_SAVE)
+    @PutMapping(ProductCommandControllerPath.PRODUCT_UPDATE)
     public RestResponse<Void> updateProduct(
             @Validated @RequestBody ProductUpdateRequest productUpdateRequest
     ) {
@@ -34,11 +42,35 @@ public class ProductCommandController {
         return RestResponse.of(HttpStatus.OK, null);
     }
 
-    @PostMapping(ProductCommandControllerPath.PRODUCT_SAVE)
+    @DeleteMapping(ProductCommandControllerPath.PRODUCT_DELETE)
     public RestResponse<Void> deleteProduct(
             @Validated @RequestBody ProductDeleteRequest productDeleteRequest
     ) {
         productCommandService.deleteProduct(productDeleteRequest);
         return RestResponse.of(HttpStatus.OK, null);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NotFoundBrandException.class)
+    public RestResponse<Void> notFoundBrandException(NotFoundBrandException e) {
+        log.error(e.getClass() + ": " + e.getMessage());
+        e.printStackTrace();
+        return RestResponse.error(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NotFoundProductException.class)
+    public RestResponse<Void> notFoundProductException(NotFoundProductException e) {
+        log.error(e.getClass() + ": " + e.getMessage());
+        e.printStackTrace();
+        return RestResponse.error(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NotFoundCategoryException.class)
+    public RestResponse<Void> notFoundCategoryException(NotFoundCategoryException e) {
+        log.error(e.getClass() + ": " + e.getMessage());
+        e.printStackTrace();
+        return RestResponse.error(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
